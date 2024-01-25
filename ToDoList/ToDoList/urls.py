1536ca7b3bcc5f14e36e1d0600abd4212995a542
dschloe <j2hoon85@gmail.com>
2024-01-25 23:20:51 +0900

    updated

--- ToDoList/ToDoList/urls.py
+++ ToDoList/ToDoList/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include('my_to_do_app.urls')), 
+    path('', include('my_to_do_app.urls')),
     path('admin/', admin.site.urls),
 ]
--- ToDoList/my_to_do_app/views.py
+++ ToDoList/my_to_do_app/views.py
@@ -1,7 +1,30 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+from .models import * 
 
-# my_to_do_app/views.py
 # Create your views here.
+# before
+'''
 def index(request):
-    return HttpResponse("my_to_do_app first page")
+   return HttpResponse("My_to_do_app first page")
+'''
+
+# after
+def index(request):
+    todos = Todo.objects.all()
+    content = {'todos' : todos}
+    return render(request, "index.html", content)
+
+def createTodo(request):
+    user_input_str = request.POST['todoContent']
+    new_todo = Todo(content = user_input_str)
+    new_todo.save()
+    return HttpResponseRedirect(reverse('index'))
+
+def deleteTodo(request):
+    delete_todo_id = request.GET['todoNum']
+    print("삭제한 todo의 id", delete_todo_id)
+    todo = Todo.objects.get(id = delete_todo_id)
+    todo.delete()
+    return HttpResponseRedirect(reverse('index'))
